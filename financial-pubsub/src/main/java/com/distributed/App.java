e3e18374d955a548cb8e39a496a93c4dafb17a68
Tim Vonsee <tim.vonsee@biqh.com>
2018-11-28 13:52:53 +0100

    Added logging and properties file

--- financial-pubsub/src/main/java/com/distributed/App.java
+++ financial-pubsub/src/main/java/com/distributed/App.java
@@ -4,18 +4,44 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import com.distributed.actors.Greeter;
 import com.distributed.actors.Printer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.Properties;
+
+import static com.distributed.properties.Tokens.ACTORSYSTEM_NAME;
+import static java.nio.file.StandardOpenOption.READ;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args ) throws IOException {
+        if(args.length < 1){
+            LOGGER.error("Missing argument for configuration file");
+            System.exit(1);
+        }
+
+        String propertiesFilePath = args[0];
+
+        Optional<Properties> oProperties = loadProperties(propertiesFilePath);
+
+        if(!oProperties.isPresent()){
+            System.exit(1);
+        }
+
+        Properties properties = oProperties.get();
+
         // Create Actor system where the actors will "live"
-        ActorSystem system = ActorSystem.create("financial-pubsub");
+        ActorSystem system = ActorSystem.create(properties.getProperty(ACTORSYSTEM_NAME));
 
         try {
             // Create actors (note the ActorRef and not actual actor objects.
@@ -43,5 +69,22 @@ public class App
         } finally {
             system.terminate();
         }
+    }
+
+    public static Optional<Properties> loadProperties(String propertiesFileName) {
+        Path propertiesPath = Paths.get(propertiesFileName);
+        if (!Files.exists(propertiesPath)) {
+            LOGGER.error("{} not found", propertiesPath);
+            return Optional.empty();
+        }
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = Files.newInputStream(propertiesPath, READ);
+            properties.load(inputStream);
+        } catch (IOException e) {
+            LOGGER.error("could not load properties: {} exception: {}", propertiesPath, e);
+            return Optional.empty();
+        }
+        return Optional.of(properties);
     }
 }
---
+++ financial-pubsub/src/main/java/com/distributed/properties/Tokens.java
@@ -0,0 +1,6 @@
+package com.distributed.properties;
+
+public class Tokens {
+    public static final String ACTORSYSTEM_NAME = "actorsystem.name";
+
+}
