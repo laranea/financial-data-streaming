3ec6586ec06fd42bd09c9d6cbb0d5a7a0b1aa183
Tim Vonsee <tim.vonsee@biqh.com>
2018-12-05 11:56:44 +0100

    load balancer (in progress)

--- financial-pubsub/src/main/java/com/distributed/actors/RoundRobinLoadbalancerActor.java
+++ financial-pubsub/src/main/java/com/distributed/actors/RoundRobinLoadbalancerActor.java
@@ -2,15 +2,48 @@ package com.distributed.actors;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-public class LoadbalancerActor extends AbstractActor {
+public class RoundRobinLoadbalancerActor extends AbstractActor {
 
+    static public Props props(List<ActorRef> actors) {
+        return Props.create(RoundRobinLoadbalancerActor.class, () -> new RoundRobinLoadbalancerActor(actors));
+    }
+
+    public RoundRobinLoadbalancerActor(List<ActorRef> actors) {
+        this.actors = actors;
+        this.rrList = actors.iterator();
+    }
+
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
     private List<ActorRef> actors;
+    private Iterator<ActorRef> rrList;
+
+    public class Broadcast {
+        public final Object objectToBroadcast;
+
+        public Broadcast(Object objectToBroadcast) {
+            this.objectToBroadcast = objectToBroadcast;
+        }
+    }
 
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder().match(Broadcast.class, broadcast -> {
+            for (ActorRef a : actors) {
+                getSelf().tell(broadcast.objectToBroadcast, a);
+            }
+        }).matchAny(o -> {
+
+            rrList.next();
+
+        }).build();
     }
 }
